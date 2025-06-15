84c0594cf99ffe1e8042bab4217825943fef59d2
GaoZQi <Ga0ZQ1@outlook.com>
2025-06-15 22:29:32 +0800

    feat: 更新路由配置，使用 hash 模式以支持更好的路由管理

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // 使用 hash 模式
   routes,
 })
 
